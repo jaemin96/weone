4dc4282f6464fa5835129ca4d98431557ce8857b
jaemin96 <lemon__96@naver.com>
2024-05-30 21:47:41 +0900

    feat: pseudo-states dependancy 설치 & storybook UI 테스트
    Github issue #9

--- src/components/Button/stories/Button.stories.ts
+++ src/components/Button/stories/Button.stories.ts
@@ -1,16 +1,19 @@
 import { Button } from './../Button';
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { action } from '@storybook/addon-actions';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
   component: Button,
   parameters: {
     layout: 'centered',
+    pseudo: { hover: true },  
   },
   tags: ['autodocs'],
-  argTypes: {},
-  args: { onClick: fn() }, 
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+  args: { onClick: action('button-click') },
 };
 
 export default meta;
@@ -18,22 +21,31 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    primary: true,
-    size:"medium",
-    children: "click",
+    primary: false,
+    size: 'medium',
+    children: 'click',
+  },
+  parameters: {
+    pseudo: { hover: true },  
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-    children: "click",
+    children: 'click',
+  },
+  parameters: {
+    pseudo: { hover: true },  
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    children: "click",
+    children: 'click',
+  },
+  parameters: {
+    pseudo: { hover: true },  
   },
 };
