aa8b7453d780304e3aa61cc96316426494469089
jaemin96 <lemon__96@naver.com>
2024-06-05 11:38:14 +0900

    feat: signin 컴포넌트 수정 - 로그인 상태에 따른 UI 변경
    Github issue #12

--- src/components/Sign/Sign-in.tsx
+++ src/components/Sign/Sign-in.tsx
@@ -1,25 +1,53 @@
-import React from "react";
+import React from 'react';
 import { Form } from '../Form';
 import { Input } from '../Input';
 import { Button } from '../Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from '../../redux/modules/actions/signActions';
+import { RootState } from '../../redux/modules';
 
 type FormData = {
-    email: string;
-    password: string;
+  id: string;
+  password: string;
 };
 
-const SignIn:React.FC = ()  => {
-    const onSubmit = (data: FormData) => {
-        console.log(data);
-    };
+const SignIn: React.FC = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.sign.user);
 
-    return (
-        <Form<FormData> onSubmit={onSubmit}>
-            <Input name="email" label="ID" required/>
-            <Input name="password" label="Password" type="password" required/>
-            <Button>Submit</Button>
-        </Form>
-    )
-}
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    const { id, password } = data;
+
+    if (id === 'admin' && password === '1234') {
+      dispatch(login({ id }));
+    } else {
+      alert('login failed!');
+    }
+  };
+
+  React.useEffect(() => {
+    console.log({ user });
+    return () => {
+      console.log('');
+    };
+  }, [user]);
+
+  return user?.isLoggedIn ? (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <span>logged in</span>
+      <span>{`id : ${user.id}`}</span>
+      <div>
+        <Button onClick={() => dispatch(logout())}>로그아웃</Button>
+      </div>
+    </div>
+  ) : (
+    <Form<FormData> onSubmit={onSubmit}>
+      <Input name="id" label="ID" required />
+      <Input name="password" label="Password" type="password" required />
+      <Button>로그인</Button>
+    </Form>
+  );
+};
 
 export default SignIn;
