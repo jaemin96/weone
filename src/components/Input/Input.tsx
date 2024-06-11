c67ff4d831abfe21a1bb53fe2f3bcdf801949202
jaemin96 <lemon__96@naver.com>
2024-06-11 15:51:13 +0900

    fix: input 컴포넌트 props 추가 및 라벨 표시 여부에 따른 UI 제어

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,16 +5,20 @@ type InputProps = {
     label: string;
     required?: boolean;
     type?: string;
+    className?: string;
+    isLabel?: boolean;
 };
 
-const Input:React.FC<InputProps> = ({ name, label, type = 'text', required = false }) => {
+const Input:React.FC<InputProps> = ({ name, label, className, isLabel = false, type = 'text', required = false }) => {
     const { register } = useFormContext();
     return (
-        <fieldset style={{border: "none"}}>
-            <label htmlFor={name}>
-                {required && <span style={{ color: "red" }}>*</span>}
-                {label}
-            </label>
+        <fieldset className={className} style={{border: "none"}}>
+            {isLabel &&
+                <label htmlFor={name}>
+                    {required && <span style={{ color: "red" }}>*</span>}
+                    {label}
+                </label>
+            }
             <input {...register(name, { required })} id={name} type={type}/>
         </fieldset>
     )
