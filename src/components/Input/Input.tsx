ca7d11d5abbaf82e757c6fbe6e5b89a880b38204
jaemin96 <lemon__96@naver.com>
2024-06-12 20:03:26 +0900

    fix: input component prop 추가 - readOnly & disabled

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,9 +7,11 @@ type InputProps = {
     type?: string;
     className?: string;
     isLabel?: boolean;
+    readOnly?: boolean;
+    disabled?: boolean;
 };
 
-const Input:React.FC<InputProps> = ({ name, label, className, isLabel = false, type = 'text', required = false }) => {
+const Input:React.FC<InputProps> = ({ name, label, className, isLabel = false, readOnly=false, disabled=false, type = 'text', required = false }) => {
     const { register } = useFormContext();
     return (
         <fieldset className={className} style={{border: "none"}}>
@@ -19,7 +21,7 @@ const Input:React.FC<InputProps> = ({ name, label, className, isLabel = false, t
                     {label}
                 </label>
             }
-            <input {...register(name, { required })} id={name} type={type}/>
+            <input {...register(name, { required })} id={name} type={type} disabled={disabled} readOnly={readOnly}/>
         </fieldset>
     )
 }
