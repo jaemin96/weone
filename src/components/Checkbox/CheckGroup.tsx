6ec231a5213d89c48ee64320107ef7d11d853e1d
jaemin <lemon__96@naver.com>
2024-06-19 16:57:35 +0900

    feat: add handler & manage state - CheckGroup component
    handleGroupCheck, handleChildCheck 핸들러 함수 추가 & useEffect 로직 추가
    CheckGroup & CheckBox 상태 동기화 수행

--- src/components/Checkbox/CheckGroup.tsx
+++ src/components/Checkbox/CheckGroup.tsx
@@ -12,17 +12,47 @@ export const CheckGroup = ({id, label, children}: CheckGroupProps) => {
     const [isCheckedAll, setIsCheckedAll] = useState<boolean>(false);
     const [childChecked, setChildChecked] = useState<{[key: string]: boolean}>({}); // {"child-check" : true | false}
 
-    // TODO: handler - group check
+    /**
+     * @param event 
+     * 부모 체크요소가 업데이트 되면 checked 값에 따라 isCheckedAll 값 상태를 업데이트하는 핸들러 함수
+     * plus) 부모 체크요소의 값에 따라 자식 체크요소들의 값 또한 업데이트 되도록 setChildChecked를 통해 자식 체크요소들 업데이트하는 로직 추가
+     * ? {} as {[key:string] : boolean} - reduce를 통해 각 child의 checked 값을 객체에 누적 저장하고 객체의 타입은 {key: true|false} 로 정의
+     */
     const handleGroupCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { checked } = event.target;
         setIsCheckedAll(checked);
+
+        // update child checked - true
+        const updatedChildChecked = React.Children.toArray(children).reduce((obj, child) => {
+            if (React.isValidElement<CheckBoxProps>(child)) {
+                obj[child.props.id] = checked;
+            }
+            return obj;
+        }, {} as {[key:string] : boolean});        
+        setChildChecked(updatedChildChecked);
     }
 
-    // TODO: handler - child check
-    const handleChildCheck = () => {}
+    /**
+     * @param id
+     * @param checked 
+     * 체크 상태가 업데이트 된 자식 요소를 기존의 자식 요소들의 상태에서 업데이트하는 핸들러 함수
+     */
+    const handleChildCheck = (id: string, checked: boolean) => {
+        setChildChecked(prevState => ({
+            ...prevState,
+            [id]: checked,
+        }));
+    }
 
-    // TODO: effect - all checked -> child not checked 
-    useEffect(() => {}, []);
+    /**
+     * allChecked type boolean - true | false
+     * allChecked - 객체 검사 후 모든 값이 true인 경우 true 그렇지 않은 경우 false
+     * 자식 체크요소가 변경될 때마다 allChecked 상태 검사
+     */
+    useEffect(() => {
+        const allChecked = Object.values(childChecked).every(checked => checked);
+        setIsCheckedAll(allChecked);
+    }, [childChecked]);
 
     return (
         <div className={['check-group'].join(' ')}>
@@ -30,10 +60,15 @@ export const CheckGroup = ({id, label, children}: CheckGroupProps) => {
                 <input type="checkbox" id={id} name={label} checked={isCheckedAll} onChange={handleGroupCheck}/>
                 {label}
             </label>
-            <div>
+            <div className='check-group-children'>
                 {React.Children.map(children, (child) => {
                     if (React.isValidElement<CheckBoxProps>(child)) {
-                      return React.cloneElement(child);
+                        return React.cloneElement(child, {
+                            checked: childChecked[child.props.id] || false,
+                            onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+                                handleChildCheck(child.props.id, event.target.checked);
+                            },
+                        });
                     }
                     return child;
                 })}
